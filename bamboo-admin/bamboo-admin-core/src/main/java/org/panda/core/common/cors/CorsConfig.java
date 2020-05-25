6795161c5fee7f8de3dcff1d59ddedd167c494e2
jvfagan <1455211171@qq.com>
2020-05-25 19:37:14 +0800

    前端跨域配置

---
+++ bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/common/cors/CorsConfig.java
@@ -0,0 +1,20 @@
+package org.panda.core.common.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author jiefangen
+ * @since JDK 1.8  2020/5/24
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedOrigins("*");
+    }
+}
