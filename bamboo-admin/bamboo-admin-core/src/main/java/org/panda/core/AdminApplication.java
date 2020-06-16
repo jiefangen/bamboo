45a66ea0a29551691a63357b9f16bc2bbf17050e
jiefangen <fangenj@gmail.com>
2020-06-16 19:50:25 +0800

    开启事务管理,日志输出路径更换

--- bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/AdminApplication.java
+++ bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/AdminApplication.java
@@ -3,6 +3,7 @@ package org.panda.core;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Application启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since JDK 1.8  2020/5/4
  **/
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("org.panda.core.modules.system.dao")
 public class AdminApplication {
     public static void main(String[] args) {
