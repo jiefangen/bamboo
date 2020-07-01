6629492a2238055034c572be2cb714a03f80e791
jiefangen <fangenj@gmail.com>
2020-07-01 23:45:08 +0800

    解决事务回滚失效问题

---
+++ bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/common/util/ApplicationContextUtil.java
@@ -0,0 +1,22 @@
+package org.panda.core.common.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextUtil implements ApplicationContextAware {
+    public static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextUtil.applicationContext = applicationContext;
+    }
+    public static Object getBean(String beanName) {
+        return applicationContext.getBean(beanName);
+    }
+    public static <T> T getBean(Class<T> type) {
+        return applicationContext.getBean(type);
+    }
+}
--- bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/modules/system/service/impl/UserServiceImpl.java
+++ bamboo-admin/bamboo-admin-core/src/main/java/org/panda/core/modules/system/service/impl/UserServiceImpl.java
@@ -16,12 +16,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
