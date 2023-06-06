a9e1df7783df496a4800708b3b6548aa84f10d86
JieFangen <jiefangen@gmail.com>
2023-06-06 20:16:55 +0800

    技术框架安全鉴权整合

--- bamboo-business/business-official/src/test/java/org/panda/business/official/test/crypto/KeyGeneratorTest.java
+++ bamboo-business/business-official/src/test/java/org/panda/business/official/test/crypto/KeyGeneratorTest.java
@@ -2,14 +2,20 @@ package org.panda.business.official.test.crypto;
 
 import org.junit.jupiter.api.Test;
 import org.panda.bamboo.common.util.lang.MathUtil;
+import org.panda.business.official.test.OfficialApplicationTest;
 import org.panda.tech.core.crypto.aes.AesEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 固定密钥生成
  *
  * @author fangen
  **/
-public class KeyGeneratorTest {
+public class KeyGeneratorTest extends OfficialApplicationTest {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     void fixedKeyGenerator() {
@@ -21,6 +27,12 @@ public class KeyGeneratorTest {
         System.out.println("encryptedText: " + encryptedText);
         String decryptSource = aesEncryptor.decrypt(encryptedText, key);
         System.out.println("decryptSource: " + decryptSource);
+    }
+
+    @Test
+    void encode() {
+        String encodedPassword = passwordEncoder.encode("123456");
+        System.out.println("encodedPassword: " + encodedPassword);
     }
 
 }
