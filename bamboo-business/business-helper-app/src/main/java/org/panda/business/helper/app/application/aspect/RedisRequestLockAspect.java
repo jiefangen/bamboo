0d6f332fca47829d972439ced68aaefa245aad36
JieFangen <jiefangen@gmail.com>
2024-08-03 12:27:40 +0800

    缓存锁封装集成优化调整

--- bamboo-business/business-helper-app/src/main/java/org/panda/business/helper/app/application/aspect/RedisRequestLockAspect.java
+++ bamboo-business/business-helper-app/src/main/java/org/panda/business/helper/app/application/aspect/RedisRequestLockAspect.java
@@ -5,12 +5,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.panda.tech.core.spec.debounce.annotation.RequestLock;
 import org.panda.tech.core.spec.debounce.support.RequestLockSupport;
+import org.panda.tech.data.redis.lock.RedisCacheLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStringCommands;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.types.Expiration;
 
 /**
  * 缓存请求锁切面
@@ -21,11 +18,11 @@ import org.springframework.data.redis.core.types.Expiration;
 @Configuration
 public class RedisRequestLockAspect extends RequestLockSupport {
 
-    private final StringRedisTemplate stringRedisTemplate;
+    private final RedisCacheLock redisCacheLock;
 
     @Autowired
-    public RedisRequestLockAspect(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
+    public RedisRequestLockAspect(RedisCacheLock redisCacheLock) {
+        this.redisCacheLock = redisCacheLock;
     }
 
     @Around("execution(public * * (..)) && @annotation(org.panda.tech.core.spec.debounce.annotation.RequestLock)")
@@ -35,10 +32,6 @@ public class RedisRequestLockAspect extends RequestLockSupport {
 
     @Override
     protected Boolean isLocked(String lockKey, RequestLock requestLock) {
-        // 使用RedisCallback接口执行set命令，设置锁键；设置额外选项：过期时间和SET_IF_ABSENT选项
-        return stringRedisTemplate.execute(
-                (RedisCallback<Boolean>) connection -> connection.set(lockKey.getBytes(), new byte[0],
-                        Expiration.from(requestLock.expire(), requestLock.timeUnit()),
-                        RedisStringCommands.SetOption.SET_IF_ABSENT));
+        return redisCacheLock.isLocked(lockKey, requestLock.expire(), requestLock.timeUnit());
     }
 }
