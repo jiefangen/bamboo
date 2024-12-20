5bef5b61827a6d878eba240de5cea2911d866801
JieFangen <jiefangen@gmail.com>
2024-12-20 10:34:31 +0800

    jenkins项目部署脚本

--- standard/assembly/deploy/jenkins-deploy.sh
+++ standard/assembly/deploy/jenkins-deploy.sh
@@ -8,15 +8,12 @@
 # 本地调试目录（绝对路径）
 LOCAL_DIR="/Users/fangen/Documents/IntelliJ_IDEA/projects/github/bamboo/bamboo-business/business-admin/target"
 
-# 项目名称
-PROJECT_NAME="business-admin"
-
 # 目标目录，根据参数决定（默认为本地目录）
 TARGET_DIR=""
 
-# 判断参数，如果第一个参数是 "jenkins"，则切换到 Jenkins 的目标目录
+# 判断参数，如果第一个参数是 "jenkins"，则切换到 Jenkins 部署的目标目录
 if [ "${1:-local}" = "jenkins" ]; then
-    TARGET_DIR="/home/$PROJECT_NAME"
+    TARGET_DIR=`pwd`
 else
     TARGET_DIR="$LOCAL_DIR"
 fi
@@ -24,7 +21,6 @@ fi
 # 输出配置信息
 echo "---------------------------------"
 echo "环境参数：${1:-local}"
-echo "项目名称：$PROJECT_NAME"
 echo "目标目录：$TARGET_DIR"
 echo "---------------------------------"
 
@@ -37,23 +33,24 @@ fi
 # 进入目标目录
 cd "$TARGET_DIR" || { echo "错误：无法进入目录 $TARGET_DIR！"; exit 1; }
 
-# 压缩包文件名
-ARCHIVE_NAME="$PROJECT_NAME-assembly.tar.gz"
-
+# 获取压缩包文件名（假设只有一个压缩包）
+ARCHIVE_NAME=$(ls *-assembly.tar.gz 2>/dev/null)
 # 检查压缩文件是否存在
 if [ ! -f "$ARCHIVE_NAME" ]; then
     echo "错误：文件 $ARCHIVE_NAME 不存在！"
     exit 1
 fi
 
-# 解压前清理旧文件
+# 从压缩包文件名中提取项目名称
+PROJECT_NAME=$(echo "$ARCHIVE_NAME" | sed -E 's/-assembly//;s/.tar.gz//')
+
+# 解压前清理旧文件夹
 echo "清理旧文件夹：$PROJECT_NAME-assembly"
 rm -rf "$PROJECT_NAME-assembly"
 
 # 解压压缩包
 echo "开始解压 $ARCHIVE_NAME..."
 tar -zxvf "$ARCHIVE_NAME"
-
 # 检查解压结果
 if [ $? -eq 0 ]; then
     echo "解压完成 $ARCHIVE_NAME"
@@ -63,14 +60,13 @@ else
 fi
 
 # 查询遍历要复制的JAR文件
-FILES_TO_COPY=$(find "$TARGET_DIR/$PROJECT_NAME-assembly/jar" -name "*.jar")
-
-# 判断是否存在 .jar 文件
-if [ -z "$FILES_TO_COPY" ]; then
-    echo "警告：未找到任何 .jar 文件，跳过复制。"
+JAR_FILES_TO_COPY=$(find "$TARGET_DIR/$PROJECT_NAME-assembly/jar" -name "*.jar")
+# 判断是否存在JAR文件
+if [ -z "$JAR_FILES_TO_COPY" ]; then
+    echo "警告：未找到任何.jar文件，跳过复制。"
 else
     # 遍历查找到的文件并复制
-    for file in $FILES_TO_COPY; do
+    for file in $JAR_FILES_TO_COPY; do
         if [ -f "$file" ]; then
             cp "$file" "$TARGET_DIR"
             echo "复制JAR文件成功：$file -> $TARGET_DIR"
@@ -80,7 +76,7 @@ else
     done
 fi
 
-# 定义一个复制文件的函数
+# 定义一个复制文件夹的函数
 copy_files() {
     SOURCE_DIR="$1"
     TARGET_DIR="$2"
@@ -89,7 +85,7 @@ copy_files() {
         mkdir -p "$TARGET_DIR"
         cp -rf "$SOURCE_DIR/"* "$TARGET_DIR"
         if [ $? -eq 0 ]; then
-            echo "复制所有文件成功：$SOURCE_DIR -> $TARGET_DIR"
+            echo "复制文件夹成功：$SOURCE_DIR -> $TARGET_DIR"
         else
             echo "复制 $SOURCE_DIR 下文件时出现错误！"
         fi
@@ -103,7 +99,6 @@ BIN_DIR="$TARGET_DIR/$PROJECT_NAME-assembly/bin"
 CONF_DIR="$TARGET_DIR/$PROJECT_NAME-assembly/conf"
 TARGET_BIN_DIR="$TARGET_DIR/bin"
 TARGET_CONF_DIR="$TARGET_DIR/conf"
-
 # 调用函数复制 bin 和 conf 目录
 copy_files "$BIN_DIR" "$TARGET_BIN_DIR"
 copy_files "$CONF_DIR" "$TARGET_CONF_DIR"
--- standard/assembly/deploy/restart-deploy.sh
+++ standard/assembly/deploy/restart-deploy.sh
@@ -5,18 +5,18 @@ TARGET_DIR="/home/business-admin" # 目标目录
 
 # 获取进程ID
 pid=`ps -ef | grep "$JAR_NAME" | grep -v grep | awk '{print $2}'`
-echo "旧服务的pid进程: $pid"
+echo "【信息】旧服务的pid进程: $pid"
 
 # 关闭已经启动的jar进程
 if [ -n "$pid" ]; then
   kill -9 $pid
 else
-  echo "旧服务未启动！"
+  echo "【警告】旧服务未启动！"
 fi
 sleep 5s
 
 # 进入目标目录
 cd "$TARGET_DIR"
 nohup java -jar "$TARGET_DIR/$JAR_NAME" > "$TARGET_DIR/nohup.out" 2>&1 &
-echo "部署脚本执行完毕"
+echo "【信息】部署脚本执行完毕"
 sleep 5s
