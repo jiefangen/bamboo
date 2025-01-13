bdc7738ad34ba8d23fafef32c489c55612a6db2a
JieFangen <jiefangen@gmail.com>
2025-01-13 17:43:47 +0800

    案例服务配置调整

---
+++ bamboo-business/business-example/src/main/java/org/panda/business/example/modules/HomeController.java
@@ -0,0 +1,10 @@
+package org.panda.business.example.modules;
+
+import io.swagger.annotations.Api;
+import org.panda.tech.core.web.controller.HomeControllerSupport;
+import org.springframework.stereotype.Controller;
+
+@Api(tags = "匿名服务问候语")
+@Controller
+public class HomeController extends HomeControllerSupport {
+}
