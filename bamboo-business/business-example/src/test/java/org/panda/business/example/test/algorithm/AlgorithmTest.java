4470569220e00e0ff397fa625454572a32046d85
JieFangen <jiefangen@gmail.com>
2025-08-20 15:09:24 +0800

    项目结构版本统一管理

--- bamboo-business/business-example/src/test/java/org/panda/business/example/test/algorithm/AlgorithmTest.java
+++ bamboo-business/business-example/src/test/java/org/panda/business/example/test/algorithm/AlgorithmTest.java
@@ -2,6 +2,7 @@ package org.panda.business.example.test.algorithm;
 
 import org.junit.jupiter.api.Test;
 import org.panda.bamboo.common.util.algorithm.AlgoChecksum;
+import org.panda.bamboo.common.util.lang.StringUtil;
 
 /**
  * 算法测试类
@@ -15,5 +16,11 @@ public class AlgorithmTest {
         AlgoChecksum algoChecksum = new AlgoChecksum(10, 10);
         String checkResult = algoChecksum.visit("Hello");
         System.out.println(checkResult);
+    }
+
+    @Test
+    void generateKey() {
+        String key = StringUtil.randomNormalMixeds(32);
+        System.out.println(key);
     }
 }
