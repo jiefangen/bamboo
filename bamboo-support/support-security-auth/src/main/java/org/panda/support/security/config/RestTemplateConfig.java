4eb69a3374d91067095504260e94e65de7db4fd3
JieFangen <jiefangen@gmail.com>
2025-09-17 11:28:39 +0800

    RestTemplate配置权限交由上层系统

---
+++ bamboo-support/support-security-auth/src/main/java/org/panda/support/security/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package org.panda.support.security.config;
+
+import org.panda.tech.core.util.http.client.RestTemplateConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 服务间调用RestTemplate配置
+ *
+ * @author fangen
+ **/
+@Configuration
+public class RestTemplateConfig extends RestTemplateConfiguration {
+}
